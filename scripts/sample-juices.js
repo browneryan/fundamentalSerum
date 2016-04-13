b640ce2f9bda09afd4570d1902a62b73fd0ef409
jsonberry <jason.s.awbrey@gmail.com>
2016-04-13 11:04:33 -0700

    Create sample data

---
+++ scripts/sample-juices.js
@@ -0,0 +1,11 @@
+module.exports = {
+  juice1 : {
+    name : 'Basic',
+    desc : 'This is so basic',
+    img : 'http://lorempixel.com/400/200',
+    price : 1000,
+    stength : '1.5 G',
+    size : '30 MG',
+    flavors : 'Apple, Goat, Rhubarb'
+  }
+}
